b5aab65d8bc8cc260063b5f75ad1e0edd623919c
OmarZahid-21i2509 <i212509@nu.edu.pk>
2025-06-15 03:56:33 +0500

    Fix: use PORT from environment for Render

--- app.py
+++ app.py
@@ -538,6 +538,6 @@ def segmentation_map():
 
     
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+ #   port = int(os.environ.get('PORT', 10000))
+ #   app.run(host='0.0.0.0', port=port)
